8b5473ec0980c71028c1938603ff6ebc6b10eaab
kurticognjen <kurticognjen@gmail.com>
2023-06-16 11:15:07 +0200

    Introduce tests for exactly 0 and exactly 2 deactivation messages for processDeactivationMessage circuit

--- circuits/ts/__tests__/ProcessDeactivationMessages.test.ts
+++ circuits/ts/__tests__/ProcessDeactivationMessages.test.ts
@@ -16,8 +16,6 @@ import {
     Keypair,
     PCommand,
     Message,
-    Ballot,
-    StateLeaf,
     DeactivatedKeyLeaf,
 } from 'maci-domainobjs'
 
@@ -27,7 +25,6 @@ import {
     IncrementalQuinTree,
     elGamalEncryptBit,
     stringifyBigInts,
-    NOTHING_UP_MY_SLEEVE,
 } from 'maci-crypto'
 
 const voiceCreditBalance = BigInt(100)
@@ -63,6 +60,7 @@ describe('ProcessDeactivationMessages circuit', () => {
         const commands: PCommand[] = []
         const H0 = BigInt('8370432830353022751713833565135785980866757267633941821328460903436894336785');
         let H;
+        let H2;
         const userKeypair = new Keypair(new PrivKey(BigInt(1)));
 
         beforeAll(async () => {
@@ -90,6 +88,94 @@ describe('ProcessDeactivationMessages circuit', () => {
             )
 
             poll = maciState.polls[pollId]
+        })
+
+        it('should return empty array hash in case no deactivation messages', async () => {
+            const salt = (new Keypair()).privKey.rawPrivKey
+
+            const messageArr = [];
+            for (let i = 0; i < maxValues.maxMessages; i += 1) {
+                messageArr.push(new Message(BigInt(0), Array(10).fill(BigInt(0))))
+            }
+
+            // ecdhKeypair.pubKey -> encPubKey
+
+            const DEACT_TREE_ARITY = 5;
+
+            const deactivatedKeys = new IncrementalQuinTree(
+                STATE_TREE_DEPTH,
+                H0,
+                DEACT_TREE_ARITY,
+                hash5,
+            )
+
+            
+            console.log(maciState.stateLeaves[1])
+            console.log(userKeypair.pubKey)
+            console.log(maciState.stateLeaves[1].asCircuitInputs())
+            
+            const maskingValues = []
+            // Pad array
+            for (let i = 0; i < maxValues.maxMessages; i += 1) {
+                maskingValues.push('1')
+            }
+
+            H = H0;
+
+            const encPubKeys = [];
+            // Pad array
+            for (let i = 0; i < maxValues.maxMessages; i += 1) {
+                encPubKeys.push(new PubKey([BigInt(0), BigInt(0)]).asCircuitInputs());
+            }
+
+            const deactivatedTreePathElements = [];
+            // Pad array
+            for (let i = 0; i < maxValues.maxMessages; i += 1) {
+                deactivatedTreePathElements.push(deactivatedKeys.genMerklePath(0).pathElements)
+            }
+            
+            const stateLeafPathElements = [];
+            // Pad array
+            for (let i = 0; i < maxValues.maxMessages; i += 1) {
+                stateLeafPathElements.push(maciState.stateTree.genMerklePath(0).pathElements)
+            }
+
+            const currentStateLeaves = [];
+            // Pad array
+            for (let i = 0; i < maxValues.maxMessages; i += 1) {
+                currentStateLeaves.push(maciState.stateLeaves[0].asCircuitInputs())
+            }
+
+            const elGamalEnc = []
+            // Pad array
+            for (let i = 0; i < maxValues.maxMessages; i += 1) {
+                elGamalEnc.push(elGamalEncryptBit(
+                    coordinatorKeypair.pubKey.rawPubKey, 
+                    BigInt(0), 
+                    BigInt(1)
+                ))
+            }
+
+            const inputs = stringifyBigInts({
+                coordPrivKey: coordinatorKeypair.privKey.asCircuitInputs(),
+                coordPubKey: coordinatorKeypair.pubKey.rawPubKey,
+                encPubKeys,
+                msgs: messageArr.map(m => m.asCircuitInputs()),
+                deactivatedTreePathElements,
+                stateLeafPathElements,
+                currentStateLeaves,
+                elGamalEnc,
+                maskingValues,
+                deactivatedTreeRoot: deactivatedKeys.root,
+                currentStateRoot: maciState.stateTree.root,
+                numSignUps: 1,
+            })
+
+            const witness = await genWitness(circuit, inputs)
+            expect(witness.length > 0).toBeTruthy()
+
+            const newMessageChainHash = await getSignalByName(circuit, witness, 'main.newMessageChainHash')
+            expect(newMessageChainHash).toEqual(H0.toString());
         })
 
         it('should process exactly 1 deactivation message', async () => {
@@ -168,8 +254,6 @@ describe('ProcessDeactivationMessages circuit', () => {
                 encPubKeys.push(new PubKey([BigInt(0), BigInt(0)]).asCircuitInputs());
             }
 
-            console.log(encPubKeys)
-
             const deactivatedTreePathElements = [deactivatedKeys.genMerklePath(0).pathElements];
             // Pad array
             for (let i = 1; i < maxValues.maxMessages; i += 1) {
@@ -217,12 +301,10 @@ describe('ProcessDeactivationMessages circuit', () => {
             expect(witness.length > 0).toBeTruthy()
 
             const newMessageChainHash = await getSignalByName(circuit, witness, 'main.newMessageChainHash')
-
-            console.log(H, newMessageChainHash)
-            return 0;
+            expect(newMessageChainHash).toEqual(H.toString());
         })
 
-        it.skip('should process exactly 2 deactivation messages', async () => {
+        it('should process exactly 2 deactivation messages', async () => {
             const salt1 = (new Keypair()).privKey.rawPrivKey;
             const salt2 = (new Keypair()).privKey.rawPrivKey;
         
@@ -268,7 +350,8 @@ describe('ProcessDeactivationMessages circuit', () => {
             }
         
             // ecdhKeypair.pubKey -> encPubKey
-            const messageHash = message1.hash(ecdhKeypair.pubKey);
+            const messageHash1 = message1.hash(ecdhKeypair.pubKey);
+            const messageHash2 = message2.hash(ecdhKeypair.pubKey);
         
             const DEACT_TREE_ARITY = 5;
         
@@ -320,28 +403,31 @@ describe('ProcessDeactivationMessages circuit', () => {
                 salt2,
             )).hash())
         
-            H = hash2([H0, messageHash])
+            H = hash2([H0, messageHash1])
+            H2 = hash2([H, messageHash2])
         
-            const encPubKeys = [ecdhKeypair.pubKey.asCircuitInputs()];
+            const encPubKeys = [ecdhKeypair.pubKey.asCircuitInputs(), ecdhKeypair.pubKey.asCircuitInputs()];
+            
             // Pad array
-            for (let i = 1; i < maxValues.maxMessages; i += 1) {
+            for (let i = 2; i < maxValues.maxMessages; i += 1) {
                 encPubKeys.push(new PubKey([BigInt(0), BigInt(0)]).asCircuitInputs());
             }
-        
-            console.log(encPubKeys)
+    
         
             const deactivatedTreePathElements = [
                 deactivatedKeys.genMerklePath(0).pathElements,
                 deactivatedKeys.genMerklePath(1).pathElements
             ];
+    
             // Pad array
             for (let i = 2; i < maxValues.maxMessages; i += 1) {
                 deactivatedTreePathElements.push(deactivatedKeys.genMerklePath(0).pathElements)
             }
             
-            const stateLeafPathElements = [maciState.stateTree.genMerklePath(stateIndex).pathElements];
+            const stateLeafPathElements = [maciState.stateTree.genMerklePath(stateIndex).pathElements, maciState.stateTree.genMerklePath(stateIndex).pathElements];
+            
             // Pad array
-            for (let i = 1; i < maxValues.maxMessages; i += 1) {
+            for (let i = 2; i < maxValues.maxMessages; i += 1) {
                 stateLeafPathElements.push(maciState.stateTree.genMerklePath(0).pathElements)
             }
         
@@ -383,9 +469,7 @@ describe('ProcessDeactivationMessages circuit', () => {
             expect(witness.length > 0).toBeTruthy()
         
             const newMessageChainHash = await getSignalByName(circuit, witness, 'main.newMessageChainHash')
-        
-            console.log(H, newMessageChainHash)
-            return 0;
+            expect(newMessageChainHash).toEqual(H2.toString());
         })
         
         it('should throw if numSignUps 0 with 1 deactivation message', async () => {
